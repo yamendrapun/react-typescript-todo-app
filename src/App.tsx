7e331d01186e61d851657d3463f983e98d3d9e99
Yamendra Pun <punyamendra@gmail.com>
2022-05-25 21:37:25 +0900

    use State Hook in App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
 import './App.css';
 
-const reminders: Reminder[] = [
-  { id: 1, title: 'Reminder 1' }
-];
-
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: 'Reminder 1' }
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
